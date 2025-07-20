88717906d008562192dceb2b85fd67bd87029644
“sandyavs” <sandyasadasivam01@gmail.com>
2025-07-20 13:50:45 -0400

    fetchYogaData

---
+++ src/Components/FetchYogaData.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useFetch from './UseFetch';
+import './FetchData.css';
+
+const FetchYogaData = () => {
+    const [data] = useFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    console.log(data);
+    return (
+        <ul className="list_data_main">
+            <h1 className='usefetch_heading'>Yoga Benefits</h1>
+            {data && data.map((e) => (
+                <li className="list_data">
+                    <h3>{e.name}</h3>
+                    <p><strong>Benefits: </strong>{e.benefits}</p>
+                    <p><strong>Duration: </strong>{e.time_duration}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default FetchYogaData;
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 const UseFetch = (url) => {
-    const [data, setData] = useState();
+    const [data, setData] = useState(null);
     useEffect(() => {
+        if(!url) return;
+
         fetch(url)
         .then((res) => res.json())
         .then((data) => setData(data))
         .catch((err) => {
             console.log('Error fetching data', err);
+            setData(null);
         });
-    }, []);
+    }, [url]); // add url as dependency
     return [data];
 }
 

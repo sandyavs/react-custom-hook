c6ef9e8f7b604018226d48f70e61c2332587bb51
“sandyavs” <sandyasadasivam01@gmail.com>
2025-07-20 13:03:51 -0400

    custom hook: implementation

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react"
+
 const UseFetch = (url) => {
-    
+    const [data, setData] = useState();
+    useEffect(() => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((err) => {
+            console.log('Error fetching data', err);
+        });
+    }, []);
+    return [data];
 }
 
 export default UseFetch
